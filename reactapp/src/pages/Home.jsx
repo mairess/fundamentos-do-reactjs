74bb0d5a4cd381701b217db5b10c0ba262c61b9a
Maires Souza <trabalho.maires@gmail.com>
2023-04-26 15:21:26 -0300

    add fragment

--- reactapp/src/pages/Home.jsx
+++ reactapp/src/pages/Home.jsx
@@ -4,7 +4,11 @@ function Home() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Lista de Presença</h1>
+    <>
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder='Digite o nome...' />
+      <button type='button'>Adicionar</button>
+    </>
   )
 }
 
