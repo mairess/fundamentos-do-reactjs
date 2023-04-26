59f4387dadd7d07e3d8ea01c4f92acaacd7c999b
Maires Souza <trabalho.maires@gmail.com>
2023-04-26 09:56:46 -0300

    update main

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Home.jsx'
+import Home from './pages/Home.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
 )
