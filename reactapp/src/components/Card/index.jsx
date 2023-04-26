9709b7bd8af57a2620e751c512d5f213f0563c8b
Maires Souza <trabalho.maires@gmail.com>
2023-04-26 16:55:58 -0300

    add components

---
+++ reactapp/src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export function Card(){
+    return(
+        <div className='card'>
+            <strong>Rodrigo Gonçalves</strong>
+            <small>10:15:24</small>
+        </div>
+    )
+}
