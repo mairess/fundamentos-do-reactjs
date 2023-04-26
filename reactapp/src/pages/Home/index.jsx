c4625ab60262098b078686f379b3bd2262c1d111
Maires Souza <trabalho.maires@gmail.com>
2023-04-26 16:11:02 -0300

    add div

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -5,11 +5,11 @@ export function Home() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <h1>Lista de Presença</h1>
       <input type="text" placeholder='Digite o nome...' />
       <button type='button'>Adicionar</button>
-    </>
+    </div>
   )
 }
 
