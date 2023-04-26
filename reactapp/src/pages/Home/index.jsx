140bcd38cd2711f1e74237557289736c3ecd5f4d
Maires Souza <trabalho.maires@gmail.com>
2023-04-26 16:12:21 -0300

    add className

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -5,7 +5,7 @@ export function Home() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className='container'>
       <h1>Lista de Presença</h1>
       <input type="text" placeholder='Digite o nome...' />
       <button type='button'>Adicionar</button>
