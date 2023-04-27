8f4c2efe4b46e0f262071d266fe7b136d7d994fe
Maires Souza <trabalho.maires@gmail.com>
2023-04-27 09:10:56 -0300

    add key property

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -34,7 +34,13 @@ export function Home() {
         </button>
 
       {
-        students.map(student => <Card name={student.name} time={student.time}/>)
+        students.map(student => (
+        <Card 
+        key={student.time}
+        name={student.name} 
+        time={student.time}
+        />
+        ))
         
       }
       
