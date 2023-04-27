d61531be9052c8817da251c65986618ada375be3
Maires Souza <trabalho.maires@gmail.com>
2023-04-27 10:35:51 -0300

    add useEffect async

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -21,14 +21,18 @@ export function Home() {
   }
 
   useEffect(() => {
-    fetch('https://api.github.com/users/mairess')
-    .then( response => response.json())
-    .then( data => {
+    async function fetchData(){
+      const response = await fetch('https://api.github.com/users/mairess')
+      const data = await response.json();
+      console.log("DADOS ===>", data);
       setUser({
         name: data.name, 
-        avatar:data.avatar_url
+        avatar:data.avatar_url,
       })
-    })
+    }
+
+    fetchData()
+  
   }, []);
 
   return (
