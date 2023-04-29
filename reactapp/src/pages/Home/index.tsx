d6b620a058e1c8fff41519f250ee3e826a8e9bf1
Maires Souza <trabalho.maires@gmail.com>
2023-04-29 10:13:36 -0300

    add ProfileResponse and type User

--- reactapp/src/pages/Home/index.tsx
+++ reactapp/src/pages/Home/index.tsx
@@ -2,10 +2,20 @@ import { useState, useEffect } from 'react'
 import './styles.css';
 import { Card, CardProps } from '../../components/Card'
 
+type ProfileResponse = {
+  name: string;
+  avatar_url: string;
+}
+
+type User = {
+  name: string;
+  avatar: string;
+}
+
 export function Home() {
   const [studentName, setStudentName] = useState();
   const [students, setStudents] = useState<CardProps[]>([]);
-  const [user, setUser] = useState({name: "", avatar: ""});
+  const [user, setUser] = useState<User>({} as User);
 
   function handleAddStudent(){
     const newStudent = {
@@ -23,11 +33,11 @@ export function Home() {
   useEffect(() => {
     async function fetchData(){
       const response = await fetch('https://api.github.com/users/mairess')
-      const data = await response.json();
-      console.log("DADOS ===>", data);
+      const data = await response.json() as ProfileResponse;
+      
       setUser({
         name: data.name, 
-        avatar:data.avatar_url,
+        avatar: data.avatar_url,
       });
     }
 
